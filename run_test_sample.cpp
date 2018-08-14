06637eee70504fe4832cdf86f78c04c6af758ecd
eunjeung <rislove418@gmail.com>
2018-08-14 13:49:32 +0900

    PM 01:49, edit

--- run_test_sample.cpp
+++ run_test_sample.cpp
@@ -27,6 +27,7 @@
 #define SIZE 24
 //#define SIZE 262144
 #define TOTAL_SIZE ((int)(SIZE+SIZE*0.2))
+#define PAGE_SIZE_NEW ((int) 8)
 
 using namespace ssd;
 
@@ -40,63 +41,34 @@ int main()
 
 	double result;
 	double start_time=0;
-	//double afterFormatStartTime = 19200;
 	int count1 = 0, count2 = 0, count3 =0;
-	//int value=1;
-	char *buff1, *buff2, *buff3;
 	int ret, ret1, ret2;
+	
+	char *buff1 = malloc(sizeof(char)*PAGE_SIZE_NEW); 
+	char *buff2, *buff3;
+	
 
-	buff1 = malloc(sizeof(char)*PAGE_SIZE);
-	buff2 = malloc(sizeof(char)*PAGE_SIZE);
-	buff3 = malloc(sizeof(char)*PAGE_SIZE);
+	/*
+	buff1 = malloc(sizeof(char)*PAGE_SIZE_NEW);
+	buff2 = malloc(sizeof(char)*PAGE_SIZE_NEW);
+	buff3 = malloc(sizeof(char)*PAGE_SIZE_NEW);
+	*/
 
-	memset(buff1, 1, sizeof(char)*PAGE_SIZE);
-	memset(buff2, 2, sizeof(char)*PAGE_SIZE);
-	memset(buff3, 3, sizeof(char)*PAGE_SIZE);
+	memset(buff1, 1, sizeof(char)*PAGE_SIZE_NEW);
+	memset(buff2, 2, sizeof(char)*PAGE_SIZE_NEW);
+	memset(buff3, 3, sizeof(char)*PAGE_SIZE_NEW);
 
 	for (int i = 0; i < SIZE; i++)
 	{
 		//long int r = random()%SIZE;
 		result = ssd -> event_arrive(WRITE, i, 1, (double)(300*i), buff1);
-		//printf("Write time: %.20lf\n", result);
-		//start_time += result;
 	}
 
 	for(int i=0;i<TOTAL_SIZE;i++){
-		ret = memcmp(page_data+i*PAGE_SIZE,buff1,(sizeof(char)*PAGE_SIZE));
+		ret = memcmp(page_data+i*PAGE_SIZE_NEW,buff1,(sizeof(char)*PAGE_SIZE_NEW));
 		if(ret==0) count1++;
 	}
 
-	/*
-	for (int i = 0; i < SIZE; i++)
-	{
-		result = ssd -> event_arrive(WRITE, i, 1, start_time, buff2);
-		//printf("Write time: %.20lf\n", result);
-		start_time += result;
-	}
-
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		result = ssd -> event_arrive(WRITE, i, 1, start_time, buff3);
-		//printf("Write time: %.20lf\n", result);
-		start_time += result;
-	}
-	*/
-	/*
-	for(int i=0;i<TOTAL_SIZE;i++){
-		ret = memcmp(page_data+i*PAGE_SIZE,&value,4);
-		if(ret==0) count1++;
-		else{
-			ret1 = memcmp(page_data+i*PAGE_SIZE,&value1,4);
-			if(ret1==0) count2++;
-			else{
-				ret2 = memcmp(page_data+i*PAGE_SIZE, &value2, 4);
-				if(ret2==0) count3++;
-			}
-		}
-	}
-	*/
 	printf("number of '1' : %d \n", count1);
 	//printf("number of '2' : %d \n", count2);
 	//printf("number of '3' : %d \n", count3);

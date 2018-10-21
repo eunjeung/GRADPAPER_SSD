974ff9d3d569c9a373ef6e8137841aaea92a76dd
eunjeung <rislove418@gmail.com>
2018-10-21 19:46:26 +0900

    181021 edit : Cassiano edit

--- flashsim/run_test7.cpp
+++ flashsim/run_test7.cpp
@@ -18,15 +18,12 @@
 #include "ssd.h"
 #include <string.h>
 #include <math.h>
-
-//#define USER_ADDRESS_SPACE (int)(ceil(NUMBER_OF_ADDRESSABLE_PAGES*0.6))
-
-#define NUMBER_OF_ADDRESSABLE_PAGES (int)(NUMBER_OF_ADDRESSABLE_BLOCKS*BLOCK_SIZE)
+ //#define USER_ADDRESS_SPACE (int)(ceil(NUMBER_OF_ADDRESSABLE_PAGES*0.6))
+ #define NUMBER_OF_ADDRESSABLE_PAGES (int)(NUMBER_OF_ADDRESSABLE_BLOCKS*BLOCK_SIZE)
 #define FILE_SIZE_A (int)(ceil(BLOCK_SIZE*0.3))
 #define FILE_SIZE_B (int)(ceil(BLOCK_SIZE*1.6))
 #define FILE_SIZE_C (int)(ceil(BLOCK_SIZE*0.7))
-
-using namespace ssd;
+ using namespace ssd;
  
  
  int main()
@@ -55,10 +52,9 @@ using namespace ssd;
 	memset(buff2, 2, sizeof(char)*PAGE_SIZE);
 	memset(buff3, 3, sizeof(char)*PAGE_SIZE);
 	memset(buff4, 4, sizeof(char)*PAGE_SIZE);
-
-
-	//#TEST_CASE_4 
+ 	//#TEST_CASE_4 
 	
+    // Writing the files A, B, and C.
 	for (int i = 0; i < FILE_SIZE_A; i++)
 	{
 		result = ssd -> event_arrive(WRITE, i, 1, (double)(300*i), buff1);
@@ -73,79 +69,64 @@ using namespace ssd;
 	{
 		result = ssd -> event_arrive(WRITE, i, 1, (double)(300*i), buff3);
 	}
-	
-	for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
-		ret = memcmp((page_data+(i*PAGE_SIZE)),buff1,(sizeof(char)*PAGE_SIZE));
-		if(ret==0) count1++;
-		else{
-			ret1 = memcmp((page_data+(i*PAGE_SIZE)), buff2, (sizeof(char)*PAGE_SIZE));
-			if(ret1==0) count2++;
-			else{
-				ret2 = memcmp((page_data+(i*PAGE_SIZE)),buff3,(sizeof(char)*PAGE_SIZE));
-				if(ret2==0) count3++;
-				else{
-					ret3 = memcmp((page_data+(i*PAGE_SIZE)),buff4,(sizeof(char)*PAGE_SIZE));
-					if(ret3==0) count4++;
-				}
-			}
-		}
-	}
- 	printf("\n-------- test 1 --------\n");
-	printf("number of '1' : %d \n", count1);
-	printf("number of '2' : %d \n", count2);
-	printf("number of '3' : %d \n", count3);	
-	printf("number of '4' : %d \n", count4);	
- 	ssd -> print_statistics();
+     for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
+        ret = memcmp((page_data+(i*PAGE_SIZE)),buff1,(sizeof(char)*PAGE_SIZE));
+        if(ret==0) {
+            count1++;
+            continue;
+        }
+        ret1 = memcmp((page_data+(i*PAGE_SIZE)), buff2, (sizeof(char)*PAGE_SIZE));
+        if(ret1==0) {
+            count2++;
+            continue;
+        }
+        ret2 = memcmp((page_data+(i*PAGE_SIZE)),buff3,(sizeof(char)*PAGE_SIZE));
+        if(ret2==0) {
+            count3++;
+            continue;
+        }
+        ret3 = memcmp((page_data+(i*PAGE_SIZE)),buff4,(sizeof(char)*PAGE_SIZE));
+        if(ret3==0) {
+            count4++;
+            continue;
+        }
+    }
+    printf("\n-------- test 1 --------\n");
+    printf("number of '1' : %d \n", count1);
+    printf("number of '2' : %d \n", count2);
+    printf("number of '3' : %d \n", count3);
+    printf("number of '4' : %d \n", count4);
+    ssd -> print_statistics();
 	
 	result = 0;
-	
-	for(int k = 2; ; k ++){
-		for (int i = FILE_SIZE_A ; i < (FILE_SIZE_A + FILE_SIZE_B); i++)
-		{
-			result += ssd -> event_arrive(WRITE, i, 1, (double)(300*i), buff4);
-		}
-		
-		count1=0;
-		count2=0;
-		count3=0;
-		count4=0;
-		
-		printf("%d %d %d %d", count1, count2, count3, count4);
-
-		for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
-			ret = memcmp((page_data+(i*PAGE_SIZE)),buff1,(sizeof(char)*PAGE_SIZE));
-			if(ret==0) count1++;		
-			else{
-				ret1 = memcmp((page_data+(i*PAGE_SIZE)),buff2,(sizeof(char)*PAGE_SIZE));
-				if(ret1==0) count2++;
-				else{
-					ret2 = memcmp((page_data+(i*PAGE_SIZE)),buff3,(sizeof(char)*PAGE_SIZE));
-					if(ret2==0) count3++;
-					else{
-						ret3 = memcmp((page_data+(i*PAGE_SIZE)),buff4,(sizeof(char)*PAGE_SIZE));
-						if(ret3==0) count4++;
-					}
-				}
-			}
-		}
-		
-		printf("\n--------- test %d -------- \n", k);
-		printf("number of '1' : %d \n", count1);
-		printf("number of '2' : %d \n", count2);
-		printf("number of '3' : %d \n", count3);	
-		printf("number of '4' : %d \n", count4);	
-		ssd -> print_statistics();
-		
-		if(count2==0) break;
-		if(count2==59) break;
-	}
-	
-	printf ("\n\n--------- result time : %lf ---------\n", result);
-	
- 	delete ssd;
+    int iterations = 0;
+     while(1) {
+        printf("it = %d\n", iterations++);
+        for (int i = FILE_SIZE_A ; i < (FILE_SIZE_A + FILE_SIZE_B); i++)
+        {
+            result += ssd -> event_arrive(WRITE, i, 1, (double)(300*i), buff4);
+        }
+         count2=0;
+         for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
+            ret1 = memcmp((page_data+(i*PAGE_SIZE)),buff2,(sizeof(char)*PAGE_SIZE));
+            if(ret1==0) {
+                count2++;
+                continue;
+            }
+        }
+         if (count2 == 0)
+            break;
+    }
+     printf ("\n\n--------- result time : %lf ---------\n", result);
+     printf("\n--------- test %d -------- \n", k);
+    printf("number of '2' : %d \n", count2);
+    printf("Number of iterations : %d \n", iterations);
+    ssd -> print_statistics();
+  	delete ssd;
 	free(buff1);
 	free(buff2);
 	free(buff3);
+    free(buff4);
 	
 	return 0;
 }

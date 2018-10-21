29fbaad9ca8854d4f2e67d9292662908172f1b31
eunjeung <rislove418@gmail.com>
2018-10-21 19:55:17 +0900

    181020 edit : Cassiano edit2

--- flashsim/run_test7.cpp
+++ flashsim/run_test7.cpp
@@ -18,12 +18,15 @@
 #include "ssd.h"
 #include <string.h>
 #include <math.h>
- //#define USER_ADDRESS_SPACE (int)(ceil(NUMBER_OF_ADDRESSABLE_PAGES*0.6))
- #define NUMBER_OF_ADDRESSABLE_PAGES (int)(NUMBER_OF_ADDRESSABLE_BLOCKS*BLOCK_SIZE)
+
+//#define USER_ADDRESS_SPACE (int)(ceil(NUMBER_OF_ADDRESSABLE_PAGES*0.6))
+
+#define NUMBER_OF_ADDRESSABLE_PAGES (int)(NUMBER_OF_ADDRESSABLE_BLOCKS*BLOCK_SIZE)
 #define FILE_SIZE_A (int)(ceil(BLOCK_SIZE*0.3))
 #define FILE_SIZE_B (int)(ceil(BLOCK_SIZE*1.6))
 #define FILE_SIZE_C (int)(ceil(BLOCK_SIZE*0.7))
- using namespace ssd;
+
+using namespace ssd;
  
  
  int main()
@@ -52,7 +55,9 @@
 	memset(buff2, 2, sizeof(char)*PAGE_SIZE);
 	memset(buff3, 3, sizeof(char)*PAGE_SIZE);
 	memset(buff4, 4, sizeof(char)*PAGE_SIZE);
- 	//#TEST_CASE_4 
+
+
+	//#TEST_CASE_4 
 	
     // Writing the files A, B, and C.
 	for (int i = 0; i < FILE_SIZE_A; i++)
@@ -69,7 +74,8 @@
 	{
 		result = ssd -> event_arrive(WRITE, i, 1, (double)(300*i), buff3);
 	}
-     for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
+
+    for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
         ret = memcmp((page_data+(i*PAGE_SIZE)),buff1,(sizeof(char)*PAGE_SIZE));
         if(ret==0) {
             count1++;
@@ -100,29 +106,41 @@
 	
 	result = 0;
     int iterations = 0;
-     while(1) {
+
+    while(1) {
         printf("it = %d\n", iterations++);
         for (int i = FILE_SIZE_A ; i < (FILE_SIZE_A + FILE_SIZE_B); i++)
         {
             result += ssd -> event_arrive(WRITE, i, 1, (double)(300*i), buff4);
         }
-         count2=0;
-         for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
+
+        count2=0;
+
+        for(int i=0;i<NUMBER_OF_ADDRESSABLE_PAGES;i++){
             ret1 = memcmp((page_data+(i*PAGE_SIZE)),buff2,(sizeof(char)*PAGE_SIZE));
             if(ret1==0) {
                 count2++;
                 continue;
             }
         }
-         if (count2 == 0)
+
+        if (count2 == 0)
             break;
+
+        if (!(it %100)) {
+            ssd.print_statistics();
+        }
+        printf("count1 : %d\n" count2);
     }
-     printf ("\n\n--------- result time : %lf ---------\n", result);
-     printf("\n--------- test %d -------- \n", k);
+
+    printf ("\n\n--------- result time : %lf ---------\n", result);
+
+    printf("\n--------- test %d -------- \n", k);
     printf("number of '2' : %d \n", count2);
     printf("Number of iterations : %d \n", iterations);
     ssd -> print_statistics();
-  	delete ssd;
+
+ 	delete ssd;
 	free(buff1);
 	free(buff2);
 	free(buff3);
